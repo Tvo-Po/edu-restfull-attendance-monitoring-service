d3b1a53352c2b9bec72c9a14d690a76b154d5ba9
Tvo-Po <maxwellgleb@gmail.com>
2022-12-17 20:46:40 +0300

    Project set up

--- RESTfull.API/Program.cs
+++ RESTfull.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<RESTfull.Infrastructure.Context>(
+    options => options.UseSqlite(
+        builder.Configuration.GetConnectionString("Database")
+    )
+);
 
 var app = builder.Build();
 
---
+++ RESTfull.Infrastructure/Data/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RESTfull.Infrastructure;
+public class Context : DbContext
+{
+    public Context(DbContextOptions<Context> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+}
